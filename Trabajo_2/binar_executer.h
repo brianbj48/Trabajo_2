299d0ef404056c85457ef1bbf4344ae85e340f09
brianbj48 <brian.ingenieria@gmail.com>
2018-03-14 00:36:53 -0300

    first bug fixed

--- Trabajo_2/binar_executer.h
+++ Trabajo_2/binar_executer.h
@@ -1,10 +1,2 @@
-<<<<<<< HEAD
 #pragma once
 int BExecuter(int election, int value1, int value2);
-<<<<<<< HEAD
-=======
-=======
-#pragma once
-int BExecuter(int eleccion, int valor1, int valor2);
->>>>>>> ebde7a4ca324ec83a2d70903f5e6038c36136ecb
->>>>>>> 8a0a14ffe63ae4cd305e18c3a33a9ced23250c73
